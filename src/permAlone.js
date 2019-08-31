06136fde5399da769343cebbac27147e81e9a469
William Mallett <mallett002@gmail.com>
2019-08-31 09:45:42 -0500

    wip adding loop for checks

--- src/permAlone.js
+++ src/permAlone.js
@@ -16,6 +16,18 @@ function getPermCount(number) {
     return count;
 }
 
+// items: a, b, c (length 3)
+// indexes: 0, 1, 2 
+function isValidPerm(arr) {
+    let isValid = true;
+
+    for (let i = 0; i < arr.length -1; i++) {
+        if (arr[i] === arr[i + 1]) isValid = false;
+    }
+
+    return isValid;
+}
+
 module.exports = (input) => {
     // 123, 213, 231, 
     // 321, 312, 132,
@@ -32,8 +44,12 @@ module.exports = (input) => {
     // keep updating arrangment of letters and incrementing a count
     // Once count reaches numberOfPerms stop.
     // swap function:
-    const swappedFirstAndSecond = swap(0, 1, baseArr);
+    // const swappedFirstAndSecond = swap(0, 1, baseArr);
     // continuously call swap, incrementing params until 2nd one is the last index of the array
     // swap(0, 1, baseArr), swap(1, 2, newArray), swap(2, 3, newArray), swap(0, 1, newArray)..
     // do that and incrementing count until === numberOfPerms. Then done building up perms.
+    const baseArrIsValid = isValidPerm(baseArr);
+    console.log({baseArr});
+    console.log({baseArrIsValid});
+
 };
