cdec58d899e6c8b8d613e42bca2edd4b55917182
William Mallett <mallett002@gmail.com>
2022-05-20 06:31:27 -0500

    more canConstruct

--- src/canConstruct.js
+++ src/canConstruct.js
@@ -1,13 +1,16 @@
 const canConstruct = (target, wordBank) => {
-    if (target === '') return true;
+    if (target === '') return true; // base case, empty string means you can safely use the wordBank to "delete" the chars.
 
-    for (let char of wordBank) {
-        const reg = new RegExp(char);
+    for (let word of wordBank) {
+        // make sure it's a prefix (starts at index 0 of the target)
+            // if it is, call canConstruct on it and get the value, return at end
+        if (target.indexOf(word) === 0) {
+            // take off the prefix, and pass the next piece to canConstruct
+            const suffix = target.slice(word.length); // ex: 'howdy'.slice('how'.length) => returns 'dy'
 
-        if (reg.test(target)) {
-            const newTarget = target.replace(reg, '');
-
-            return canConstruct(newTarget, wordBank);
+            if (canConstruct(suffix, wordBank)) { // if you find one, return true early, otherwise don't return until outside the loop
+                return true;
+            }
         }
     }
 
--- test/canConstruct.spec.js
+++ test/canConstruct.spec.js
@@ -27,4 +27,34 @@ describe('canConstruct function', () => {
 
         expect(actual).toBeFalsy();
     });
+
+    test('returns true with larger wordBank pieces', () => {
+        const target = 'abcdef';
+        const wordBank = ['ab', 'abc', 'cd', 'def', 'abcd'];
+
+        const start = performance.now();
+        const actual = canConstruct(target, wordBank);
+        console.log(performance.now() - start);
+
+        expect(actual).toBeTruthy();
+    });
+
+    test('handles large inputs', () => {
+        const target = 'eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeef';
+        const wordBank = [
+            'e',
+            'ee',
+            'eee',
+            'eeee',
+            'eeeee',
+            'eeeeee',
+            'eeeeeee'
+        ];
+
+        const start = performance.now();
+        const actual = canConstruct(target, wordBank);
+        console.log(performance.now() - start);
+
+        expect(actual).toBeTruthy();
+    });
 });
