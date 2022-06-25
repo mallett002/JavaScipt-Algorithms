438e0a0aa177aba60c01676eb72f8c79a4d9994e
William Mallett <mallett002@gmail.com>
2022-06-25 14:14:37 -0500

    finish hasCycle

--- src/linked-lists/linked-list-cycle.js
+++ src/linked-lists/linked-list-cycle.js
@@ -1,16 +1,22 @@
 
- function ListNode(val) {
-     this.val = val;
-     this.next = null;
- }
- 
+function ListNode(val) {
+    this.val = val;
+    this.next = null;
+}
 
 /**
  * @param {ListNode} head
  * @return {boolean}
  */
-const hasCycle = (head) => {
-    return false
+
+const hasCycle = (head, visited = new Set()) => {
+    if (!head) return false;
+    if (!head.next) return false;
+    if (visited.has(head)) return true;
+
+    visited.add(head);
+
+    return hasCycle(head.next, visited);
 };
 
 module.exports = {
--- test/linked-lists/linked-list-cycle.spec.js
+++ test/linked-lists/linked-list-cycle.spec.js
@@ -1,18 +1,53 @@
 const {ListNode, hasCycle} = require('../../src/linked-lists/linked-list-cycle');
 
 describe('hasCycle function', () => {
-    test('returns true when there is a loop', () => {
-        // [3,2,0,-4], pos = 1
+    test('returns true when "[1,2], pos = 0"', () => {
+        const first = new ListNode(1);
+        const second = new ListNode(2);
+
+        first.next = second;
+        second.next = first;
+
+        const actual = hasCycle(first);
+
+        expect(actual).toBe(true);
+    });
+
+    test('returns true when "[3,2,0,-4], pos = 1"', () => {
         const first = new ListNode(3);
         const second = new ListNode(2);
-        first.next = second;
         const third = new ListNode(0);
-        second.next = third;
         const last = new ListNode(-4);
+
+        first.next = second;
+        second.next = third;
+        third.next = last;
         last.next = second;
 
         const actual = hasCycle(first);
 
         expect(actual).toBe(true);
+    });
+
+    test('returns false when "[3,2,0,-4], pos = -1"', () => {
+        const first = new ListNode(3);
+        const second = new ListNode(2);
+        const third = new ListNode(0);
+        const last = new ListNode(-4);
+
+        first.next = second;
+        second.next = third;
+
+        const actual = hasCycle(first);
+
+        expect(actual).toBe(false);
+    });
+
+    test('returns false when "head = [1], pos = -1"', () => {
+        const first = new ListNode(1);
+
+        const actual = hasCycle(first);
+
+        expect(actual).toBe(false);
     });
 });
