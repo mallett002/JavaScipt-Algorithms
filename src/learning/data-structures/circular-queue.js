21a516d24b03a9a9142338dd969588b66b8b1fd2
William Mallett <mallett002@gmail.com>
2022-06-28 06:26:24 -0500

    circle-queue working

--- src/learning/data-structures/circular-queue.js
+++ src/learning/data-structures/circular-queue.js
@@ -6,22 +6,17 @@ class CircularQueue {
 
     constructor(size) {
         this._maxSize = size;
-        this._front = -1;
+        this._front = 0;
         this._rear = -1;
         this._items = [];
     }
 
     isFull() {
-        if (this._front === 0 && this._rear === this._maxSize - 1 || // full queue and hasn't looped back to start index
-            this._front === this._rear + 1) { // if the rear is now one less than the front, it's full again
-            return true;
-        }
-
-        return false;
+        return this._items.length === this._maxSize;
     }
 
     isEmpty() {
-        return this._front === -1 && this._rear === -1;
+        return this._items.length === 0;
     }
 
     peek() {
@@ -34,15 +29,8 @@ class CircularQueue {
             return;
         }
 
-        this._items.unshift(item);
-
-        this._front++;
-
-        if (this._rear === this._maxSize - 1) {
-            this._rear = 0;
-        } else {
-            this._rear++;
-        }
+        this._rear++;
+        this._items[this._rear % this._maxSize] = item;
     }
 
     dequeue() {
@@ -50,14 +38,19 @@ class CircularQueue {
             console.log('The queue is already empty');
             return;
         }
+        
+        const item = this._items[this._front % this._maxSize];
 
-        if (this._front === this._maxSize - 1) {
-            this._front = 0;
-        } else {
-            this._front++;
-        }
+        this._items[this._front % this._maxSize] = null;
 
-        return this._items.splice(this._front, 1);
+        this._front++;
+
+        return item;
+    }
+
+    printQueue() {
+        console.log('queue: ', this._items);
+        console.log('front: ', this._items[this._front]);
     }
 }
 
@@ -66,23 +59,23 @@ const queue = new CircularQueue(5);
 
 console.log('Adding an item');
 queue.enqueue(1);
-console.log('peeking: ', queue.peek());
+queue.printQueue();
 
 console.log('Adding 4 more');
 for (let index = 2; index <= 5; index++) {
     queue.enqueue(index);
 }
-console.log('peeking after 4 added: ', queue.peek());
+queue.printQueue();
 
 console.log('attempting to add beyond maxItems limit: ');
 queue.enqueue(6);
 
-// console.log('Dequeueing: ');
-// queue.dequeue();
-// console.log('peeking: ', queue.peek());
+console.log('Dequeueing 1: ');
+queue.dequeue();
+queue.printQueue();
 
-// console.log('dequeueing the rest');
-// for (let index = 0; index < 5; index++) {
-//     queue.dequeue();
-// }
-// console.log('peeking now: ', queue.peek());
+console.log('dequeueing the rest');
+for (let index = 0; index < 5; index++) {
+    queue.dequeue();
+}
+queue.printQueue();
