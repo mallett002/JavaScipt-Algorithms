bcb4c4dcfbc38081475430422a081acceb179261
William Mallett <mallett002@gmail.com>
2022-09-05 13:48:23 -0500

    best sum to ts

--- src/bestSum.ts
+++ src/bestSum.ts
@@ -1,21 +1,25 @@
 // Return the shortest list of numbers that add up to the target sum
 // loop over numbers, keeping track of shortest list, if new list is shorter, set that to be the new shortest list
 // if the shortest list was never reasigned, it will be null (no numbers added up to the target)
-const bestSum = (targetSum, numbers, memo = {}) => {
+interface Imemo {
+   number?: boolean
+}
+
+export const bestSum = (targetSum: number, numbers: number[], memo: Imemo = {}) => {
    if (targetSum in memo) return memo[targetSum];
    if (targetSum === 0) return [];
    if (targetSum < 0) return null;
 
-   let shortestCombo = null;
+   let shortestCombo: number[] | null = null;
 
-   for (const number of numbers) {
-      if (number === 0) continue;
+   for (const num of numbers) {
+      if (num === 0) continue;
 
-      const diff = targetSum - number;
+      const diff = targetSum - num;
       const result = bestSum(diff, numbers, memo);
 
       if (result) {
-         const newCombo = [...result, number];
+         const newCombo = [...result, num];
 
          if (!shortestCombo || shortestCombo && newCombo.length < shortestCombo.length) {
             shortestCombo = [...newCombo];
@@ -28,5 +32,3 @@ const bestSum = (targetSum, numbers, memo = {}) => {
 
    return shortestCombo;
 };
-
-module.exports = bestSum;
