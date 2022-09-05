a9d59ce9071f3a1b467517016b7fa80531243d28
William Mallett <mallett002@gmail.com>
2022-09-05 18:41:08 -0500

    two sum recursion

--- src/algorithms-75/array/two-sum.ts
+++ src/algorithms-75/array/two-sum.ts
@@ -1,7 +1,8 @@
-// Returns the nums in the list that add up to the target
+// Returns the indexes of the nums in the list that add up to the target
+
 export const twoSumBruteForce = (nums: number[], target: number): number[] => {
-    for (let i = 0; i <= nums.length; i++) {
-        for (let j = 0; j <= nums.length; j++) {
+    for (let i = 0; i <= nums.length; i++) {                            // loop over nums
+        for (let j = 0; j <= nums.length; j++) {                        // loop over it again (matrix)
             if (i === j) {
                 continue;
             }
@@ -15,8 +16,37 @@ export const twoSumBruteForce = (nums: number[], target: number): number[] => {
     return [];
 };
 
-const twoSumRecursion = (nums, target) => {
-    if (target === 0) return [];
+// Keep map of {val: index}
+// loop through
+// if target - val (diff) is a key, we've found our 2 values that add up to the target
+export const twoSumRecursion = (nums: number[], target: number): number[] => {
+    const valToIndex = {}; // key value pairs that add up to the target
 
-    // implement me :)
+    for (let i = 0; i < nums.length; i++) {
+        const diff = target - nums[i];
+
+        if (diff in valToIndex) { // if (target - val) is key in valToIndex; Already seen this diff, if seen twice, we have our 2 indexes.
+            // return their 2 indexes:
+            return [valToIndex[diff], i]; // return the other's index and this ones index
+        }
+
+        valToIndex[nums[i]] = i; // set currentVal: index
+    }
+
+    return [];
 };
+
+// if difference was a current val previously
+/*
+// {val: index}
+{
+    3: 0,
+    2: 1,
+    4: 2
+}
+
+// will return [1, 2]
+
+
+if (target - nums[i] in hash) {
+*/
--- test/algorithms-75/array/two-sum.spec.ts
+++ test/algorithms-75/array/two-sum.spec.ts
@@ -1,13 +1,15 @@
-import {twoSumBruteForce} from '../../../src/algorithms-75/array/two-sum';
+import {twoSumBruteForce, twoSumRecursion} from '../../../src/algorithms-75/array/two-sum';
 
-describe('twoSum function', () => {
+describe('twoSum', () => {
     test('when [2,7], target = 0, returns []', () => {
         const target = 0;
         const items = [2,7];
 
         const actual = twoSumBruteForce(items, target);
+        const actualRecurssive = twoSumRecursion(items, target);
 
         expect(actual).toStrictEqual([]);
+        expect(actualRecurssive).toStrictEqual([]);
     });
 
     test('when [2,7,11,15], target = 9, returns [0, 1]', () => {
@@ -15,8 +17,10 @@ describe('twoSum function', () => {
         const items = [2,7,11,15];
 
         const actual = twoSumBruteForce(items, target);
+        const actualRecurssive = twoSumRecursion(items, target);
 
         expect(actual).toStrictEqual([0, 1]);
+        expect(actualRecurssive).toStrictEqual([0, 1]);
     });
 
     test('when [3,2,4], target = 6, returns [1, 2]', () => {
@@ -24,16 +28,20 @@ describe('twoSum function', () => {
         const items = [3, 2, 4];
 
         const actual = twoSumBruteForce(items, target);
+        const actualRecurssive = twoSumRecursion(items, target);
 
         expect(actual).toStrictEqual([1, 2]);
+        expect(actualRecurssive).toStrictEqual([1, 2]);
     });
-
+    
     test('when nums = [3,3], target = 6, returns [0, 1]', () => {
         const target = 6;
         const items = [3, 3];
 
         const actual = twoSumBruteForce(items, target);
+        const actualRecurssive = twoSumRecursion(items, target);
 
         expect(actual).toStrictEqual([0, 1]);
+        expect(actualRecurssive).toStrictEqual([0, 1]);
     });
 });
