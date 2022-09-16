811d681e7b5d38d2116d486b834881bada4a338e
William Mallett <mallett002@gmail.com>
2022-09-16 06:27:37 -0500

    make it work

--- src/algorithms-75/array/sell-stock.ts
+++ src/algorithms-75/array/sell-stock.ts
@@ -1,31 +1,21 @@
 export const maxProfit = (prices: number[]): number => {
-    // Find the lowest price
-    const lowest = Math.min(...prices);
-    const highest = Math.max(...prices);
+    let largestProfit = 0;
 
-    if (lowest === highest) {                       // all the same price
-        return 0;
-    }
+    for (let i = 0; i < prices.length - 1; i++) {
+        const price = prices[i];
+        const smallest = Math.min(...prices);
+        const rest = prices.slice(i + 1);
+        const largest = Math.max(...rest);
+        const diff = largest - price;
 
-    const iOfLowest = prices.indexOf(lowest);
-    const iOfHighest = prices.indexOf(highest);
-
-    if (iOfHighest > iOfLowest) {                  // if lowest comes before highest
-        return highest - lowest;
-    } else {
-        let greatestProfit = 0;
-
-        for (let i = 0; i < prices.length - 1; i++) {
-            const hopefulLowest = prices[i];
-            const rest = prices.slice(i + 1, prices.length);
-            const nextHighest = Math.max(...rest);
-            const diff = nextHighest - hopefulLowest;
-
-            if (diff > greatestProfit) {
-                greatestProfit = diff;
-            }
+        if (price === smallest && diff > largestProfit) {
+            return diff;
         }
 
-        return greatestProfit;
+        if (diff > largestProfit) {
+            largestProfit = diff;
+        }
     }
+
+    return largestProfit;
 };
