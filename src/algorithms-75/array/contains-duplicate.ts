a566b0a639a99cc599e861f03c12b9ce23094974
William Mallett <mallett002@gmail.com>
2022-09-19 05:52:15 -0500

    no need to spread and make it more concise

--- src/algorithms-75/array/contains-duplicate.ts
+++ src/algorithms-75/array/contains-duplicate.ts
@@ -1,8 +1,2 @@
-// Time complexity O(n)
-// spreading an array is linear
-
-export const doContainsDuplicate = (nums: number[]): boolean => {
-    const sansDupes = [...new Set(nums)];
-
-    return sansDupes.length !== nums.length;
-};
+export const doContainsDuplicate = (nums: number[]): boolean =>
+    new Set(nums).size !== nums.length;
