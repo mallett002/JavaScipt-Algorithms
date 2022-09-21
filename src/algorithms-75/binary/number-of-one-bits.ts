346b29eb5b8f5ec03f9ee980cc51ce915a332db8
William Mallett <mallett002@gmail.com>
2022-09-21 06:21:01 -0500

    one way of doing it

--- src/algorithms-75/binary/number-of-one-bits.ts
+++ src/algorithms-75/binary/number-of-one-bits.ts
@@ -1,3 +1,7 @@
-export const hammingWeight = () => {
 
+
+export const hammingWeight = (n: number): number => {
+    const binary = n.toString(2);
+    
+    return binary.split('').filter((it) => it !== '0').length;
 };
--- test/algorithms-75/binary/number-of-one-bits.spec.ts
+++ test/algorithms-75/binary/number-of-one-bits.spec.ts
@@ -3,19 +3,19 @@ import {hammingWeight} from '../../../src/algorithms-75/binary/number-of-one-bit
 describe('hammingWeight', () => {
     // https://leetcode.com/problems/number-of-1-bits/
     it('should return 3 when given 00000000000000000000000000001011', () => {
-        const result = hammingWeight(00000000000000000000000000001011);
+        const result = hammingWeight(11);
 
         expect(result).toBe(3);
     });
 
     it('should return 1 when given 00000000000000000000000010000000', () => {
-        const result = hammingWeight(00000000000000000000000010000000);
+        const result = hammingWeight(128);
 
         expect(result).toBe(1);
     });
 
     it('should return 31 when given 11111111111111111111111111111101', () => {
-        const result = hammingWeight(11111111111111111111111111111101);
+        const result = hammingWeight(4294967293);
 
         expect(result).toBe(31);
     });
