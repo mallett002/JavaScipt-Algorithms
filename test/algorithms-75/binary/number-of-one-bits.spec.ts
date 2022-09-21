28560645c236738abdcaf5d3eb591cac68cf7dfe
William Mallett <mallett002@gmail.com>
2022-09-21 05:59:55 -0500

    make failing test

---
+++ test/algorithms-75/binary/number-of-one-bits.spec.ts
@@ -0,0 +1,22 @@
+import {hammingWeight} from '../../../src/algorithms-75/binary/number-of-one-bits'
+
+describe('hammingWeight', () => {
+    // https://leetcode.com/problems/number-of-1-bits/
+    it('should return 3 when given 00000000000000000000000000001011', () => {
+        const result = hammingWeight(00000000000000000000000000001011);
+
+        expect(result).toBe(3);
+    });
+
+    it('should return 1 when given 00000000000000000000000010000000', () => {
+        const result = hammingWeight(00000000000000000000000010000000);
+
+        expect(result).toBe(1);
+    });
+
+    it('should return 31 when given 11111111111111111111111111111101', () => {
+        const result = hammingWeight(11111111111111111111111111111101);
+
+        expect(result).toBe(31);
+    });
+});
