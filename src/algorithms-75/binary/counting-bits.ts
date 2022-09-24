bcd459cb06d86c995652d711abc26178aeb6f71f
William Mallett <mallett002@gmail.com>
2022-09-24 08:03:56 -0500

    brute force

--- src/algorithms-75/binary/counting-bits.ts
+++ src/algorithms-75/binary/counting-bits.ts
@@ -1,6 +1,17 @@
 // Return an array of the number of 1 bits for each number countin up to "n"
 export function countBits(n: number): number[] {
-    return [0];
+    let bits: number[] = [];
+
+    while (n >= 0) {
+        const numOfOnesForBinaryNum: number = n.toString(2)
+            .split('')
+            .reduce((accum, curr) => accum + parseInt(curr), 0);
+        
+        bits = [numOfOnesForBinaryNum, ...bits];
+        n--;
+    }
+
+    return bits;
 }
 
 /*
@@ -19,5 +30,10 @@ My work:
 2: 010  -> "one 1"  -> [0, 1, 1]
 3: 011  -> "2 ones" -> [0, 1, 1, 2]
 4: 100  -> "1 one"  -> [0, 1, 1, 2, 1]
-5: 1100 -> "2 ones" -> [0, 1, 1, 2, 1, 2]
+5: 101 -> "2 ones" -> [0, 1, 1, 2, 1, 2]
+6: 110 -> "2 ones" -> [0, 1, 1, 2, 1, 2, 2]
+7: 111 -> "3 ones" -> [0, 1, 1, 2, 1, 2, 2, 3]
+8: 1000
+
 */
+
--- test/algorithms-75/binary/counting-bits.spec.ts
+++ test/algorithms-75/binary/counting-bits.spec.ts
@@ -5,12 +5,18 @@ describe('countBits', () => {
     it('should return [0,1,1] when given 2', () => {
         const result = countBits(2);
 
-        expect(result).toBe([0,1,1]);
+        expect(result).toStrictEqual([0,1,1]);
     });
 
     it('should return [0,1,1,2,1,2] when given 5', () => {
         const result = countBits(5);
 
-        expect(result).toBe([0,1,1,2,1,2]);
+        expect(result).toStrictEqual([0,1,1,2,1,2]);
+    });
+
+    it('should return [0,1,1,2,1,2,2,3] when given 65', () => {
+        const result = countBits(7);
+
+        expect(result).toStrictEqual([0,1,1,2,1,2,2,3]);
     });
 });
