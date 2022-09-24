5aab4c2d387931ef174e44950af3eeac77b31cf3
William Mallett <mallett002@gmail.com>
2022-09-24 09:08:41 -0500

    working on brute force

--- src/algorithms-75/binary/counting-bits.ts
+++ src/algorithms-75/binary/counting-bits.ts
@@ -1,5 +1,5 @@
 // Return an array of the number of 1 bits for each number countin up to "n"
-export function countBits(n: number): number[] {
+export function firstTryCountBits(n: number): number[] {
     let bits: number[] = [];
 
     while (n >= 0) {
@@ -14,26 +14,49 @@ export function countBits(n: number): number[] {
     return bits;
 }
 
-/*
-Input: n = 2
-Output: [0,1,1]
-Explanation:
-0 --> 0
-1 --> 1
-2 --> 10
----------------
-My work:
-2: 010
+// https://www.youtube.com/watch?v=RyBM56RIWrM
+export function bruteForce(n: number): number[] {
+    // start at n
+    // figure out how many 1s: (n % 2)
+        // put that in the result array
+    // move to left one: (n / 2)
 
-0: 0 ones -> [0]
-1: 001  -> "one 1"  -> [0, 1]
-2: 010  -> "one 1"  -> [0, 1, 1]
-3: 011  -> "2 ones" -> [0, 1, 1, 2]
-4: 100  -> "1 one"  -> [0, 1, 1, 2, 1]
-5: 101 -> "2 ones" -> [0, 1, 1, 2, 1, 2]
-6: 110 -> "2 ones" -> [0, 1, 1, 2, 1, 2, 2]
-7: 111 -> "3 ones" -> [0, 1, 1, 2, 1, 2, 2, 3]
-8: 1000
+    let result: number[] = [];
+
+    while (n) {
+        const amountOfOnes = (n % 2);
+        console.log({amountOfOnes});
+        
+        result.unshift(amountOfOnes);
+
+        n = Math.floor(n / 2);
+    }
+
+    return result;
+}
+/*
+Brute force solution
+3: 011
+    3 % 2 = 1 (How many ones in the 1's spot), put it in the array [1]
+    3 / 2 = 1 (move to the next ones spot) 1 is new number
+
+    1 % 2 = 1 (How many ones in the 1's spot), put it in the array [1, 1]
+    1 / 2 = 0 (move to the next ones spot) 0 is new number
+
+    0 % 2 = 0 (How many ones in the 1's spot), put it in front of the array [0, 1, 1]
+    0 / 2 = 0 (we're done)
+*/
+
+/*
+0 through 7 in binary:
+0:  000  
+1:  001  
+2:  010  
+3:  011  
+4:  100  
+5:  101  
+6:  111  
+7:  1000 
 
 */
 
--- test/algorithms-75/binary/counting-bits.spec.ts
+++ test/algorithms-75/binary/counting-bits.spec.ts
@@ -1,21 +1,21 @@
-import {countBits} from '../../../src/algorithms-75/binary/counting-bits';
+import {firstTryCountBits, bruteForce} from '../../../src/algorithms-75/binary/counting-bits';
 
 describe('countBits', () => {
     // https://leetcode.com/problems/number-of-1-bits/
-    it('should return [0,1,1] when given 2', () => {
-        const result = countBits(2);
+    it('should return [0,1,1] when given 3', () => {
+        const result = bruteForce(3);
 
-        expect(result).toStrictEqual([0,1,1]);
+        expect(result).toStrictEqual([0, 1, 1, 2]);
     });
 
     it('should return [0,1,1,2,1,2] when given 5', () => {
-        const result = countBits(5);
+        const result = bruteForce(5);
 
         expect(result).toStrictEqual([0,1,1,2,1,2]);
     });
 
     it('should return [0,1,1,2,1,2,2,3] when given 65', () => {
-        const result = countBits(7);
+        const result = bruteForce(7);
 
         expect(result).toStrictEqual([0,1,1,2,1,2,2,3]);
     });
