ac34304a3d2b5194b7325dd87fa7d186ea54c5ce
William Mallett <mallett002@gmail.com>
2022-09-24 15:25:32 -0500

    build out brute force

--- src/algorithms-75/binary/counting-bits.ts
+++ src/algorithms-75/binary/counting-bits.ts
@@ -14,22 +14,29 @@ export function firstTryCountBits(n: number): number[] {
     return bits;
 }
 
+function buildCountOfTotalOnes(n: number): number {
+    let count = 0;
+    
+    // determine if last digit is a 1: (n % 2)
+    while (n) {
+        count += (n % 2);      // determine if last digit is a one
+        n = Math.floor(n / 2); // move to the right to look at the next digit
+    }
+    
+    return count;
+}
+
 // https://www.youtube.com/watch?v=RyBM56RIWrM
 export function bruteForce(n: number): number[] {
-    // start at n
-    // figure out how many 1s: (n % 2)
-        // put that in the result array
-    // move to left one: (n / 2)
-
     let result: number[] = [];
 
-    while (n) {
-        const amountOfOnes = (n % 2);
-        console.log({amountOfOnes});
-        
-        result.unshift(amountOfOnes);
-
-        n = Math.floor(n / 2);
+    for (let i = 0; i <= n; i++) {
+        if (i === 0) {
+            result.push(0);
+        } else {
+            const total = buildCountOfTotalOnes(i);
+            result.push(total);
+        }
     }
 
     return result;
