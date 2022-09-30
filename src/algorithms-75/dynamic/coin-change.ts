2fac4713221fc5bea659a0d2b75f7bb485b26b64
William Mallett <mallett002@gmail.com>
2022-09-30 06:47:45 -0500

    get coin change somewhat working

--- src/algorithms-75/dynamic/coin-change.ts
+++ src/algorithms-75/dynamic/coin-change.ts
@@ -1,3 +1,18 @@
-export function retrieveFewestCoins(coins: number[], amount: number): number {
-    return 0;
+// Top down approach (Depth first search)
+export function retrieveFewestCoins(coins: number[], amount: number, cache = {}): number {
+    if (amount < 0) return Infinity;
+    if (amount === 0) return 0;
+    if (amount in cache) return cache[amount];
+
+    let min = Infinity;
+
+    for (const coin of coins) {
+        const leftOver = amount - coin;
+        const result = retrieveFewestCoins(coins, leftOver, cache) + 1;
+
+        if (result < min) min = result;
+    }
+
+    cache[amount] = min;
+    return cache[amount] === Infinity ? -1 : cache[amount];
 }
--- test/algorithms-75/dynamic/coin-change.spec.ts
+++ test/algorithms-75/dynamic/coin-change.spec.ts
@@ -1,29 +1,42 @@
 import {retrieveFewestCoins} from '../../../src/algorithms-75/dynamic/coin-change';
 
-describe('climbing-stairs', () => {
+describe('coin-change', () => {
     // In how many distinct ways can you climb to the top?
-    // https://leetcode.com/problems/climbing-stairs/
-    it('should return 2 when given 2', () => {
-        const result = climbStairs(2);
+    // https://leetcode.com/problems/coin-change/
+
+    it('should return -1 when coins = [8], amount = 1', () => {
+        const result = retrieveFewestCoins([8], 1);
+
+        expect(result).toBe(-1);
+    });
+
+    it('should return 3 when coins = [1], amount = 1', () => {
+        const result = retrieveFewestCoins([1], 1);
+
+        expect(result).toBe(1);
+    });
+    
+    it('should return 3 when coins = [1,2,5], amount = 3', () => {
+        const result = retrieveFewestCoins([1, 2, 5], 3);
 
         expect(result).toBe(2);
     });
     
-    it('should return 3 when given 3', () => {
-        const result = climbStairs(3);
+    it('should return 3 when coins = [1,2,5], amount = 11', () => {
+        const result = retrieveFewestCoins([1, 2, 5], 11);
 
         expect(result).toBe(3);
     });
+    
+    it('should return -1 when coins = [2], amount = 3', () => {
+        const result = retrieveFewestCoins([2], 3);
 
-    it('should return 5 when given 4', () => {
-        const result = climbStairs(4);
-
-        expect(result).toBe(5);
+        expect(result).toBe(-1);
     });
 
-    it('should return 1134903170 when given 44', () => {
-        const result = climbStairs(44);
+    it('should return 0 when coins = [1], amount = 0', () => {
+        const result = retrieveFewestCoins([1], 0);
 
-        expect(result).toBe(1134903170);
+        expect(result).toBe(0);
     });
 });
