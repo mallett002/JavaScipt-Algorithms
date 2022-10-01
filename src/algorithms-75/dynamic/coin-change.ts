99f302a2243f9fa9e2d9aa783aa35c8ba839e008
William Mallett <mallett002@gmail.com>
2022-10-01 07:05:15 -0500

    some notes

--- src/algorithms-75/dynamic/coin-change.ts
+++ src/algorithms-75/dynamic/coin-change.ts
@@ -1,25 +1,25 @@
 // Top down approach (Depth first search)
-export function retrieveFewestCoins(coins: number[], amount: number, cache = {}): number {
-    if (amount in cache) return cache[amount];
+export function retrieveFewestCoins(coins: number[], amount: number): number {
     if (amount === 0) return 0;
 
     let minCoins = Infinity;
 
-    for (const coin of coins) {
-        if (coin <= amount) {
-            const result = retrieveFewestCoins(coins, amount - coin, cache);
+    for (const coin of coins) {                 // for each coin
+        if (coin <= amount) {                   // if you can successfuly take money without taking more than amount
+            let result = retrieveFewestCoins(coins, amount - coin);
 
-            if (result !== -1) {
-                minCoins = Math.min(minCoins, result + 1);
+            if (result !== -1) {                // if we were able to take money down to 0 (result !== -1)
+                result += 1;                    // add one coin to the result since we used it
+                if (result < minCoins) {        // if our result is less than current minCoins, set new minCoins
+                    minCoins = result;
+                }
             }
         }
     }
 
-    if (minCoins === Infinity) {
-        cache[amount] = -1;
-        return -1;
+    if (minCoins === Infinity) {                // if we made it here without finding new mincoins, we didn't get to 0 deducting
+        return -1;                              // and never able to set a new min coins.           
     }
 
-    cache[amount] = minCoins;
-    return minCoins;
+    return minCoins;                            // return result of minCoins at the end
 }
