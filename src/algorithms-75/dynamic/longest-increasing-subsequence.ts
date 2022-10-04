71a52b286bfe648fa78ead72a32280b92836d6b8
William Mallett <mallett002@gmail.com>
2022-10-04 06:45:31 -0500

    first attempt

--- src/algorithms-75/dynamic/longest-increasing-subsequence.ts
+++ src/algorithms-75/dynamic/longest-increasing-subsequence.ts
@@ -1,3 +1,28 @@
 export function lengthOfLIS(nums: number[]): number {
-    return 4;
-}
+    if (nums.length === 1) return 1;
+
+    if (nums.length === 2) {
+        if (nums[0] < nums[1]) {
+            return 2;
+        }
+
+        return 1;
+    }
+
+    let shortestSequence = nums;
+
+    for (let i = 1; i < nums.length - 1; i++) {
+        const current = nums[i];
+
+        if (current >= nums[i - 1]) {
+            shortestSequence = nums.slice(i);
+        }
+        if (current >= nums[i + 1]) {
+            shortestSequence = nums.slice(i + 1);
+        }
+
+        lengthOfLIS(shortestSequence);
+    }
+
+    return shortestSequence.length;
+} 
