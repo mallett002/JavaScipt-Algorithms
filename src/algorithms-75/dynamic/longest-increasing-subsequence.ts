779f70505a7cde1d8e836b9d43b072d4dd65b538
William Mallett <mallett002@gmail.com>
2022-10-08 07:04:12 -0500

    get this working

--- src/algorithms-75/dynamic/longest-increasing-subsequence.ts
+++ src/algorithms-75/dynamic/longest-increasing-subsequence.ts
@@ -2,7 +2,7 @@
 
 
 
-export function lengthOfLIS(nums: number[]): number { // [1]
+export function myFirstAttempt(nums: number[]): number { // [1]
     const cache: number[] = [];
 
     for (let i = nums.length - 1; i >= 0; i--) {
@@ -17,6 +17,27 @@ export function lengthOfLIS(nums: number[]): number { // [1]
 
     return Math.max(...cache);
 } 
+
+// Fastest way to do this
+export function lengthOfLIS(nums: number[]): number {
+    let dp: number[] = []; // need to set every val to 1
+
+    for (let i = nums.length - 1; i >= 0; i--) {
+        dp[i] = 1; // initially set each cache value to one. Using it to compare
+
+        for (let j = i + 1; j < nums.length; j++) {
+            if (nums[i] < nums[j]) {
+                // Set to largest of: itself and 1 + value after it (1 + becuase we're including nums[i])
+                dp[i] = Math.max(dp[i], 1 + dp[j]);
+            }
+        }
+    }
+
+    return Math.max(...dp);
+}
+
+
+
 
 
 /*
