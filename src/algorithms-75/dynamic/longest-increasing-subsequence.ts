131871219e2081de3f1ef9d1c03ed36ad993358d
William Mallett <mallett002@gmail.com>
2022-10-08 07:15:57 -0500

    retry it and add notes

--- src/algorithms-75/dynamic/longest-increasing-subsequence.ts
+++ src/algorithms-75/dynamic/longest-increasing-subsequence.ts
@@ -1,7 +1,3 @@
-
-
-
-
 export function myFirstAttempt(nums: number[]): number { // [1]
     const cache: number[] = [];
 
@@ -36,13 +32,13 @@ export function lengthOfLIS(nums: number[]): number {
     return Math.max(...dp);
 }
 
-
-
-
-
 /*
+Solution:
 [1, 2, 4, 3]
-                            Start
-                0         1       2         3
-        
+
+Start looping backwards to build the tabulation chart
+dp[3] =     1       (just one bc it's the only one so far)
+dp[2] =     1       (dp[2] > dp[3], so still just one)
+dp[1] =     2       (dp[1] = 1 + dp[2], because dp[1] < dp[2])
+dp[0] =     3       (dp[0] = 1 + dp[1], because dp[0] < dp[1])
 */
