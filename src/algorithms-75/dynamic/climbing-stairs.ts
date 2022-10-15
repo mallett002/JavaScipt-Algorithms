a30bd516b74bcaecb024d40c9e90e9d9f88f68d2
William Mallett <mallett002@gmail.com>
2022-10-15 08:01:58 -0500

    tabulation solution for climbStairs

--- src/algorithms-75/dynamic/climbing-stairs.ts
+++ src/algorithms-75/dynamic/climbing-stairs.ts
@@ -26,3 +26,24 @@ export function climbStairs(n: number, memo: IMemo = {}): number {
 
     return count;
 }
+
+export function tabulationClimbStairs(n: number): number {
+    const dp = [0, 1, 2];
+
+    for (let i = 3; i <= n; i++) {
+        dp[i] = dp[i - 1] + dp[i - 2];
+    }
+
+    return dp[n];
+}
+
+/*
+DP solution:
+  0
+  1 => (1)  dp[1] = 1
+  2 => (2)  dp[2] = 2
+  3 => (3)  dp[3 - 1] + dp[3 - 2]
+  4 => (5)  dp[4 - 1] + dp[4 - 2]
+  5 => (8)  dp[5 - 1] + dp[5 - 2]
+
+*/
