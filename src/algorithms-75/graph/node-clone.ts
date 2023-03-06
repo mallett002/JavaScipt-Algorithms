0b8ef45e7caa34b4e7f0e16d61dfb58934c1d661
William Mallett <mallett002@gmail.com>
2023-03-06 05:50:07 -0600

    insert in node-clone

--- src/algorithms-75/graph/node-clone.ts
+++ src/algorithms-75/graph/node-clone.ts
@@ -23,26 +23,78 @@ export function markVisitedAndCheckNeighbors(node, copy, visitedCopies) {
     });
 }
 
+function insert(intervals: number[][], newInterval: number[]): number[][] {
+    // create new list
+    // loop over intervals
+    // if newInterval comes before, add it before, then add the rest, return the rest
+    // if newInterval comes after, add curr, dont add newInterval yet bc might need to merge with something later
+    // else not b4 nor after, needs to merge
+        // merge : lowest start and hightest end of the curr & newInterval
+    
+    // after looping, if make it here, we didn't insert newInterval, so we need to insert it
+    // we didn't insert it yet, becuase above, we only inserted if it comes before curr
+    // return your list
+
+    const result: number[][] = [];
+
+    for (let i = 0; i < intervals.length; i++) {
+        const curr = intervals[i];
+
+        if (newInterval[1] < curr[0]) {
+            const rest = intervals.slice(i, intervals.length);
+
+            return [newInterval, ...rest];
+        }
+
+        if (curr[1] < newInterval[0]) {
+            result.push(curr);
+        } else {
+            newInterval = [Math.min(curr[0], newInterval[0]), Math.max(curr[1], newInterval[1])];
+        }
+    }
+
+    result.push(newInterval);
+
+    return result;
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function bfs() {
+
+}
+
 function cloneGraph(node) {
     if (!node) return null;
+    queue = new Queue())
 
-    const visitedCopies = [];
-    const copy = new MyNode(node.val);
-
-    markVisitedAndCheckNeighbors(node, copy, visitedCopies);
-
-    return copy;
 }
 
 
 
-// start a queue    
-// put 1 in queue
-// start queueing process (while nothing left in the queue)
-// take the first item
-    // process it
-    // mark it as visited
-    // look at its neighbors
-    // if they haven't been visited & not currently in the queue, put them in the queue
+
 
 
