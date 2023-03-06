8a107f4d093270283512771980ef5c624ff20d57
William Mallett <mallett002@gmail.com>
2023-03-06 06:11:44 -0600

    pass the next test

--- src/algorithms-75/string/longest-repeating-character-replacement.ts
+++ src/algorithms-75/string/longest-repeating-character-replacement.ts
@@ -1,3 +1,9 @@
+/**
+ * @param {string} s
+ * @param {number} k
+ * @return {number}
+ */
+
 function determineMostOccur(str) {
     const tested = [];
     let mostOccur = 0;
@@ -20,18 +26,46 @@ function determineMostOccur(str) {
     return letter;
 }
 
+//    3   7  11
+// AAGDJWKDSKKD
+function findNextIndex(start, str, letter) {
+    // if just starting and first char is "letter", return 0
+    if (start === 0 && str[0] === letter) return 0;
+
+    // get the substring
+    const substr = str.substring(start + 1, str.length);
+
+    // find the next index
+    const nextIndex = substr.indexOf(letter);
+
+    // figure out the real index with full string
+    if (nextIndex >= 0) {
+        return nextIndex + start + 1; // add in the index of where starting + (1 bc we're not including the current index of "letter")
+    }
+
+    return -1;
+    
+}
+
 function doOperations(str, limit, letter) {
     let largest = 0;
     let replacements = 0;
     let currCount = 0;
     
     for (let i = 0; i < str.length; i++) {
-        // find the next "letter"
-        const letterIndex = str.indexOf(letter);
-        console.log({letter, atIndex: letterIndex});
+        // set i to be the next index of "letter"
+
+        i = findNextIndex(i, str, letter);
+
+        if (i === -1) {
+            break;
+        }
+
+        console.log({letter, atIndex: i});
 
         // look backwards from letter:
-        let substrBefore = str.substring(i, letterIndex + 1);
+        // TODO: redo this:
+        let substrBefore = str.substring(0, i + 1);
 
         for (let j = substrBefore.length - 2; j >= 0 && replacements <= limit; j--) {
             
@@ -63,7 +97,7 @@ function doOperations(str, limit, letter) {
         replacements = 0;
 
         // Look forwards from letter:
-        let substrAfter = str.substring(letterIndex, str.length);
+        let substrAfter = str.substring(i, str.length);
 
         console.log({substrBefore, substrAfter})
         for (let j = 1; j <= substrAfter.length && replacements <= limit; j++) {
